680d5763de4b00ef1d5fe58af31ed66517e5af7c
maxie7 <maxie7@protonmail.com>
2023-03-05 09:45:22 +0100

    Cors problem ... wip

--- src/api/api.js
+++ src/api/api.js
@@ -4,6 +4,10 @@ const instance = axios.create({
   baseURL: 'https://itunes.apple.com'
 });
 
+const instanceCors = axios.create({
+  baseURL: 'https://aquamarine-pastelito-b19eb5.netlify.app'
+});
+
 export const API = {
   async requestPodcasts () {
     const response = await instance.get('/us/rss/toppodcasts/limit=100/genre=1310/json')
@@ -13,9 +17,16 @@ export const API = {
   },
 
   async requestChapters (id) {
-    const response = await instance.get(`/lookup?id=${id}&media=podcast&entity=podcastEpisode&limit=20`)
+    const response = await instanceCors.get(`/chapters?id=${id}&media=podcast&entity=podcastEpisode&limit=20`)
       .catch(error => error);
 
     return response;
   }
+
+  // async requestChapters (id) {
+  //   const response = await instance.get(`/lookup?id=${id}&media=podcast&entity=podcastEpisode&limit=20`)
+  //     .catch(error => error);
+  //
+  //   return response;
+  // }
 };
